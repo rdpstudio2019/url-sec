5b55dc8efa76fb7e9066a4c95d38b96d7cd6dfd1
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2022-02-24 22:17:36 +0800

    Create usec.js

--- usec.js
+++ usec.js
@@ -1,10 +1,10 @@
 db = {
-    "https://192link.com/": "疑似色情内容"
+    "https://192link.com/": "此站点存在疑似色情内容"
 }
 
 function usec(url){
     if (db.hasOwnProperty(url)){
-        return db[url];
+        return "RDPStudio网址安全中心提醒您：" + db[url];
     } else {
         return "ok";
     }
